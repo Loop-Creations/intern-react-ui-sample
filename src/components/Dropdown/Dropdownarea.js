f9d2313c9287ec3fab346d2ce3e76e145d87f060
Aparna003 <aparnashankar004@gmail.com>
2021-06-27 19:20:21 +0530

    modifying contents

--- src/components/Dropdown/Dropdownarea.js
+++ src/components/Dropdown/Dropdownarea.js
@@ -1,35 +1,35 @@
 import { useState } from "react"
 import "../../components/Dropdown/Dropdownarea.css"
 
+function Dropdownarea({ selected, setSelected }) {
+    const [isActive, setIsActive] = useState(false);
+    const options = ['Cx Demand', 'Missing Item in RO', 'Range Building', 'Periodical Replenishment'];
+    const clickHandler = (value) => {
+        setSelected(value);
+        setIsActive(false);
+    }
+    return ( <
+        div className = "dropdown" >
+        <
+        div className = "dropdown-button"
+        onClick = {
+            (e) => setIsActive(!isActive) } >
+        <
+        p > Cx Demand / Missing Item in RO / Range Building < /p> <
+        span className = "fas fa-caret-down" > < /span> <
+        /div> <
+        label > Order Type < /label> {
+            isActive &&
+                <
+                div className = "dropdown-content" > {
+                    options.map(option => < div className = "dropdown-item"
+                        onCLick = { clickHandler } > { option } < /div>)
+                    } <
+                    /div>
+                } <
+                /div>
+        );
+    }
 
-function Dropdownarea({selected,setSelected }) {
-  const [isActive, setIsActive] = useState(false);
-  const options = ['Cx Demand', 'Missing Item in RO', 'Range Building','Periodical Replenishment']
-  return (
-    <div className="dropdown">
-      <div className="dropdown-button" onClick={(e) =>
-        setIsActive(!isActive)}>
-        Cx Demand / Missing Item in RO / Range Building / Periodical Replenishment
-        <span className="fas fa-caret-down"></span>
-      </div>
-      {isActive && (
-        < div className="dropdown-content">
-          {options.map(option => (
-            <div
-              onCLick={(e) => {
-                setSelected(option)
-              setIsActive(false)
-            }}
-              className="dropdown-item">
-              {option}
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
-}
 
-
-export default Dropdownarea
-
+    export default Dropdownarea
--- src/components/Dropdown/Dropdownarea2.js
+++ src/components/Dropdown/Dropdownarea2.js
@@ -2,31 +2,38 @@ import { useState } from "react"
 import "../../components/Dropdown/Dropdownarea.css"
 
 function Dropdownarea2({ selected, setSelected }) {
-  const [isActive, setIsActive] = useState(false);
-  const options = ['IW', 'RO', 'VO', 'WH', 'OT']
-  return (
-    <div className="dropdown">
-      <div className="dropdown-button" onClick={(e) =>
-        setIsActive(!isActive)}>
-        IW / RO / VO / WH / OT
-        <span className="fas fa-caret-down"></span>
-      </div>
-      {isActive && (
-        < div className="dropdown-content">
-          {options.map(option => (
-            <div
-              onCLick={(e) => {
-                setSelected(option)
-                setIsActive(false)
-              }}
-              className="dropdown-item">
-              {option}
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+    const [isActive, setIsActive] = useState(false);
+    const options = ['IW', 'RO', 'VO', 'WH', 'OT']
+    return ( <
+        div className = "dropdown" >
+        <
+        div className = "dropdown-button"
+        onClick = {
+            (e) =>
+            setIsActive(!isActive)
+        } >
+        IW / RO / VO / WH / OT <
+        span className = "fas fa-caret-down" > < /span> <
+        /div> <
+        label > Destination Type < /label> {
+            isActive && ( <
+                div className = "dropdown-content" > {
+                    options.map(option => ( <
+                        div onCLick = {
+                            (e) => {
+                                setSelected(option)
+                                setIsActive(false)
+                            }
+                        }
+                        className = "dropdown-item" > { option } <
+                        /div>
+                    ))
+                } <
+                /div>
+            )
+        } <
+        /div>
+    );
 }
 
 export default Dropdownarea2
